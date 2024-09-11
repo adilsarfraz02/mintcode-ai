2313cb3ba5a9315d7bba663fdef11a3d3d2f0ca0
Muhammad ADil <126439037+adilsarfraz02@users.noreply.github.com>
2024-09-11 23:46:39 +0500

    Create GoogleAnalytics.js

---
+++ app/GoogleAnalytics.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-LL4DNJ15ZD`}
+      />
+
+      <Script id='' strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LL4DNJ15ZD', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
