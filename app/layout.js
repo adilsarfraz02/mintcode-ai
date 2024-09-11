4e82329a4dd4b7fd35c6423bd35e070bbd0e48f3
Muhammad ADil <126439037+adilsarfraz02@users.noreply.github.com>
2024-09-10 19:52:04 -0700

    update seo

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,12 +15,14 @@ const geistMono = localFont({
 
 export const metadata = {
   title: "MintCode AI | A helpful assistant",
-  description: "MintCode AI is a helpful assistant that can answer questions and describe images.",
-    image: "/mintcode-ai.png",
-    url: "https://mintcode-ai.vercel.app",
-    type: "website",
-    siteName: "MintCode AI",
-    locale: "en_US",
+  description:
+    "MintCode AI is a helpful assistant that can answer questions and describe images.",
+  image:
+    "https://img.playbook.com/OWvUgCQ3-IG1atVeSvx6_yIzlll-TBxnaXlS5Ih7hEE/Z3M6Ly9wbGF5Ym9v/ay1hc3NldHMtcHVi/bGljLzRhOGI4NTc5/LTc1NmQtNDExOC05/YzJhLTc1MDQwODgy/YzQ2Yw",
+  url: "https://mintcode-ai.vercel.app",
+  type: "website",
+  siteName: "MintCode AI",
+  locale: "en_US",
 };
 
 export default function RootLayout({ children }) {
@@ -29,14 +31,14 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <ThemeProvider
+        <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
