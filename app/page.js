5873c38e61b388ab0448ea9ee58bae7c65d27622
Muhammad ADil <126439037+adilsarfraz02@users.noreply.github.com>
2024-09-14 06:28:48 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -171,7 +171,7 @@ export default function ChatInterface() {
                     </div>
                     <div className='space-y-2'>
                       {message.role === "assistant" ? (
-                        <MDXRemote
+                        <MDXRemote className="prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white"
                           {...message.content}
                           components={components}
                         />
