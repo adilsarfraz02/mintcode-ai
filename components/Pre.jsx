aa40887a85d101bc8219c6c4f260f2617a74b59a
Muhammad ADil <129647437+Azhan52@users.noreply.github.com>
2024-09-18 04:31:25 -0700

    update copybtn

--- components/Pre.jsx
+++ components/Pre.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { Clipboard, ClipboardCheck } from "lucide-react";
 import { toast } from "react-hot-toast";
 import { Button } from "@/components/ui/button";
@@ -6,14 +6,16 @@ import { Button } from "@/components/ui/button";
 export const Pre = ({ children }) => {
     const [copied, setCopied] = React.useState(false);
 
-    const handleCopy = () => {
-        const codeToCopy = children.props.children.trim();
-        navigator.clipboard.writeText(codeToCopy).then(() => {
-            setCopied(true);
-            toast.success("Code copied to clipboard!");
-            setTimeout(() => setCopied(false), 2000); // Reset copy status after 2 seconds
-        });
-    };
+    const handleCopy = useCallback(() => {
+        const codeToCopy = typeof children.props.children === 'string' ? children.props.children.trim() : '';
+        if (codeToCopy) {
+            navigator.clipboard.writeText(codeToCopy).then(() => {
+                setCopied(true);
+                toast.success("Code copied to clipboard!");
+                setTimeout(() => setCopied(false), 2000); // Reset copy status after 2 seconds
+            });
+        }
+    }, [children]);
 
     useEffect(() => {
         const button = document.querySelector('.copy-button');
@@ -25,14 +27,14 @@ export const Pre = ({ children }) => {
                 button.removeEventListener('touchstart', handleCopy);
             }
         };
-    }, []);
+    }, [handleCopy]);
 
     return (
         <div className='relative'>
             <pre className='bg-gray-800 text-white p-4 rounded-lg'>{children}</pre>
             <Button
                 onClick={handleCopy}
-                className='absolute top-2 right-2 text-gray-400 hover:text-gray-100 copy-button'>
+                className='absolute top-2 right-2 text-gray-400 hover:text-gray-800 copy-button'>
                 {copied ? (
                     <ClipboardCheck className='h-4 w-4' />
                 ) : (
